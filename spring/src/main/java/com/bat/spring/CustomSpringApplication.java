2d63abb805b3d221fb7dc80ce80a3122d7ee669c
ZhengYu <zhengy@gato.cn>
2019-12-11 11:53:06 +0800

    Spring

---
+++ spring/src/main/java/com/bat/spring/CustomSpringApplication.java
@@ -0,0 +1,17 @@
+package com.bat.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring 整理
+ *
+ * @author ZhengYu
+ * @version 1.0 2019/12/11 9:29
+ **/
+@SpringBootApplication
+public class CustomSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomSpringApplication.class, args);
+    }
+}
